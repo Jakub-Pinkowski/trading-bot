7d648e5a0f34dcd97b057941076f5e9a594ac546
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2025-04-01 00:32:21 +0200

    Refactor: move `has_contract` to `orders_utils.py`
    Moved the `has_contract` function from `orders.py` to `orders_utils.py` for better modularity and code reusability. Updated import statements accordingly to reflect this change.

--- app/utils/ibkr_utils/orders_utils.py
+++ app/utils/ibkr_utils/orders_utils.py
@@ -1,5 +1,24 @@
-from app.utils.api_utils import api_post
+from app.utils.api_utils import api_get, api_post
 from config import BASE_URL
+
+
+def has_contract(conid):
+    endpoint = f"portfolio/positions/{conid}"
+
+    try:
+        positions = api_get(BASE_URL + endpoint)
+        # Iterate through each account's positions
+        for position_list in positions.values():
+            if isinstance(position_list, list):
+                for position in position_list:
+                    # Check if the position matches the conid and has a non-zero quantity
+                    if position.get("conid") == conid and position.get("position", 0) != 0:
+                        return True
+        return False  # conid either does not exist or position is zero
+
+    except Exception as err:
+        print(f"Unexpected error checking contract: {err}")
+        return False
 
 
 def suppress_messages(message_ids):
