123ed16b0e655b75f0a68cdf58c6bce0429e931f
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2025-04-03 22:54:06 +0200

    Update save_to_csv to handle appending to existing files
    Modified the save_to_csv function to check for file existence before writing. If the file exists, data is appended without headers; otherwise, a new file is created with headers. This ensures proper handling of data logging scenarios.

--- app/utils/file_utils.py
+++ app/utils/file_utils.py
@@ -19,4 +19,12 @@ def save_file(data, file_path):
 
 def save_to_csv(data, file_path):
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
-    data.to_csv(file_path, index=False)
+
+    # Check if the file exists to determine header inclusion
+    if os.path.isfile(file_path):
+        # Append data without headers
+        data.to_csv(file_path, mode='a', index=False, header=False)
+    else:
+        # Write new CSV with headers
+        data.to_csv(file_path, mode='w', index=False, header=True)
+
