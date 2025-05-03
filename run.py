f30961d53e0d7a792fb2dbd3f31a64bef4cd8053
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2025-05-03 14:54:05 +0200

    Enable conditional analysis execution based on DEBUG flag
    Imported the DEBUG setting from the config and modified the script to run the analysis only when DEBUG is set to true. This ensures analysis is executed only in development or debug environments.

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from config import PORT
+from config import DEBUG, PORT
 from app.analysis.analysis_runner import run_analysis
 from app.routes.webhook import webhook_blueprint
 from app.services.ibkr.connection import start_ibkr_scheduler
@@ -11,8 +11,9 @@ app.register_blueprint(webhook_blueprint)
 # Tickle the API every 60 seconds
 start_ibkr_scheduler()
 
-# Run analytics
-# analysis = run_analysis()
+# Run analysis if DEBUG is true
+if DEBUG:
+    run_analysis()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT)
