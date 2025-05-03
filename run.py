d67114749e5a27c97121ee45699802a5c975d815
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2025-05-03 14:58:53 +0200

    Reorder imports in run.py to follow PEP 8 guidelines
    Moved import of DEBUG and PORT to comply with PEP 8 which recommends grouping standard library imports, third-party imports, and local imports separately. No functional change was introduced.

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from flask import Flask
 
-from config import DEBUG, PORT
 from app.analysis.analysis_runner import run_analysis
 from app.routes.webhook import webhook_blueprint
 from app.services.ibkr.connection import start_ibkr_scheduler
+from config import DEBUG, PORT
 
 app = Flask(__name__)
 app.register_blueprint(webhook_blueprint)
@@ -15,4 +15,5 @@ if __name__ == '__main__':
     # Run analysis only if in debug mode
     if DEBUG:
         run_analysis()
+
     app.run(host='0.0.0.0', port=PORT)
