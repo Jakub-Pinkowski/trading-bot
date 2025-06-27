0cc2c7d4575f774aaafac8ada7493b81779bc59d
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2025-06-27 23:58:55 +0200

    Reorganize and update TODOs in `structure.yaml` for clarity. Remove redundant debug print from `BaseStrategy`.

--- app/backtesting/strategies/base_strategy.py
+++ app/backtesting/strategies/base_strategy.py
@@ -31,9 +31,6 @@ class BaseStrategy:
         df = self.generate_signals(df)
         trades = self._extract_trades(df, switch_dates)
 
-        for trade in trades:
-            print(trade)
-
         return trades
 
     def add_indicators(self, df):
